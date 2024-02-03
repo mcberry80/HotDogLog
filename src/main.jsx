244bd9ff4e0ed1476b72af85138e768441987017
Michael Berry <mcberry@gmail.com>
2024-02-03 15:02:47 -0500

    reorganize file structure and introduce react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,22 @@
 
-import App from './App';
+import Root from './Routes/root';
+import * as React from 'react';
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 
-const container = document.getElementById('app');
-const root = createRoot(container); 
-root.render(<App />);
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+    },
+  ]);
+
+  createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
